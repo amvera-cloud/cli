bbb9396074714c224cb54428b2d34f29a9122686
Timur Kim <kimutir@gmail.com>
2025-06-16 18:17:28 +1100

    feature: test install for mac

--- amvera-install.sh
+++ amvera-install.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "Hello world"
+set -e
+
+VERSION="v1.0.0"
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+TAR_URL="https://github.com/amvera-cloud/cli/releases/download/v0.9.20/amvera-macos-x64.zip"
+
+echo "Downloading $TAR_URL..."
+curl -L "$TAR_URL" -o amvera-macos-x64.zip
+
+echo "Extracting..."
+tar -xzf amvera-macos-x64.zip
+
+echo "Installing to /usr/local/bin..."
+chmod +x amvera
+sudo mv amvera /usr/local/bin/amvera
+
+echo "Installation complete. Run 'amvera help' to get started."
